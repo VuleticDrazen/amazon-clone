5652f6dc6eb942c6c7909b47b8d921c4fd90061e
Drazen Vuletic <drazen.vuletic@amplitudo.me>
2023-04-24 10:58:38 +0200

    Initial commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import MainLayout from "@/Layouts/MainLayout.vue";
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <MainLayout></MainLayout>
+</template>
