2c69c3a64371bb0630e240e45f744cfdd3b5219e
Drazen Vuletic <drazen.vuletic@amplitudo.me>
2023-04-25 13:29:58 +0200

    Add vue3 carousel into Dashboard.vue

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
 
