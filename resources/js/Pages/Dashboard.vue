5233f6151f627c23178236f63550c2313147df68
Drazen Vuletic <drazen.vuletic@amplitudo.me>
2023-04-26 13:39:37 +0200

    Add categories section

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -12,7 +12,7 @@ import {Carousel, Navigation, Slide} from "vue3-carousel";
         <Carousel :items-to-show="1" :wrap-around="true">
             <Slide v-for="slide in 10" :key="slide">
                 <div class="carousel__item">
-                    <img src="https://via.placeholder.com/1500x350.png/004466?text=Testing" alt="">
+                    <img src="https://via.placeholder.com/3000x1200.png/004466?text=Testing" alt="">
                 </div>
             </Slide>
 
@@ -20,6 +20,27 @@ import {Carousel, Navigation, Slide} from "vue3-carousel";
                 <Navigation/>
             </template>
         </Carousel>
+        <div class="relative -mt-[300px]">
+            <div class="flex relative z-10 m-4">
+                <div class="bg-white p-2 mx-2 text-md w-full text-center">
+                    You are on Amazon clone
+                    <span class="underline text-teal-600 cursor-pointer">Click here to buy me a coffee</span>
+                </div>
+            </div>
+            <div class="grid grid-cols-3 m-4 z-10 relative">
+                <div class="p-1.5 flex">
+                    <div class="bg-white p-5">
+                        <div class="text-2xl font-extrabold flex">Computers</div>
+                        <div class="flex">
+                            <img class="object-fill" src="https://via.placeholder.com/1097x756.png/" alt="">
+                        </div>
+                        <div class="pt-3 -mb-2 text-teal-800 font-bold hover:underline hover:text-red-400 cursor-pointer">
+                            See more
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </MainLayout>
 </template>
 <style>
